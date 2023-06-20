230d4b190bb92e42d910b28aa2bead4392fb0d29
Luiz Althoff <luizfernando.althoff@gmail.com>
2023-06-20 15:22:19 -0300

    import do mongoose, dotenv e alteração dos middlewares no app.js

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,25 @@
 import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user-routes";
 
+// isso serve para que, ao rodar o app, ele se comporte como o express
 const app = express();
 
+// middlewares (conexão)
+app.use(express.json());
+app.use("/user", userRouter);
+
+// conexão com banco de dados mongodb através da biblioteca mongoose, e utiliza a chave secreta que está no .env
 mongoose
   .connect(
     `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cineufsc.h5lzjpb.mongodb.net/?retryWrites=true&w=majority`
   )
   .then(() =>
+  // após, o servidor fica escutando na porta 5000 e manda uma mensagem no console confirmando que está rodando
     app.listen(5000, () =>
       console.log("Conectado ao servidor e servidor está funcionando")
     )
   )
+  // qualquer erro nessa etapa aparece no log
   .catch((e) => console.log(e));
