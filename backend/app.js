5861473a19a143a6ed6516edbc154f9676dfdd96
Luiz Althoff <luizfernando.althoff@gmail.com>
2023-06-20 10:01:24 -0300

    node conectado ao mongodb e criação do modelo User

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,14 @@
 import express from "express";
 
 const app = express();
+
+mongoose
+  .connect(
+    `mongodb+srv://admin:${process.env.MONGODB_PASSWORD}@cineufsc.h5lzjpb.mongodb.net/?retryWrites=true&w=majority`
+  )
+  .then(() =>
+    app.listen(5000, () =>
+      console.log("Conectado ao servidor e servidor está funcionando")
+    )
+  )
+  .catch((e) => console.log(e));
