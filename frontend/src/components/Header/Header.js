dd3aa040cc0e158182d5563780d6e6bdd71beb87
Kelvin Santos <kelvin.santos@valtech.com>
2023-06-21 11:44:01 -0300

    header tabs

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -14,24 +14,26 @@ import {
 const dummyArray = ["Teste","Teste1"];
 
 const Header = () => {
+    const [value, setValue] = useState(0)
   return (
-    <AppBar>
+    <AppBar sx={{bgcolor:"#2b2d42"}}> 
         <Toolbar>
                 <Box width={'20%'}>
                     <MovieIcon/>
                 </Box>
-                <Box width="50%" marginRight={"auto"} marginLeft="auto">
+                <Box width="30%" marginRight={"auto"} marginLeft="auto">
                   <Autocomplete
                     freeSolo
                     options={dummyArray.map((option) => option)}
                     renderInput={(params) => (
-                      <TextField variant="standard" 
-                      {...params} label="Search Acrros Multiple Movies"/>
+                      <TextField sx={{ input: {color:"white"}}} variant="standard" 
+                      {...params} 
+                      placeholder="Search Acrros Multiple Movies"/>
                     )}
                   />
                 </Box> 
                 <Box display="flex">
-                      <Tabs textColor ="inherit" indicatorColor="secondary" value={1} >
+                      <Tabs textColor ="inherit" indicatorColor="secondary" value={value} onChange={(e,val)=>setValue(val)} >
                         <Tab label="Movies"/>
                         <Tab label="Admin" />
                         <Tab label="Auth" />
