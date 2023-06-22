4e3ef5f3125b6a8c5f609025bd4c508cd3dd123f
Kelvin Santos <kelvin.santos@valtech.com>
2023-06-22 10:04:24 -0300

    arrumado erro da sessão

--- frontend/src/components/Auth/Admin.js
+++ frontend/src/components/Auth/Admin.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import AuthForm from './AuthForm';
 import { sendAdminAuthRequest } from '../../api-helpers/api-helpers';
+import { useDispatch } from 'react-redux';
+import { adminActions } from '../../store';
 
 const Admin = () => {
+  // Retorna a referencia para o dispatch da redux store
+  const dispatch = useDispatch()
   /*Recupera dos dados preenchidos no AuthForm para esse componente */
   const getData = (data) => {
     console.log("Admin", data);
     sendAdminAuthRequest(data.inputs)
     .then((res)=> console.log(res))
+    .then(()=>dispatch(adminActions.login()))
     .catch(err => console.log(err));
 
   };
--- frontend/src/components/Auth/Auth.js
+++ frontend/src/components/Auth/Auth.js
@@ -1,13 +1,18 @@
 import React from 'react'
 import AuthForm from './AuthForm';
 import { sendUserAuthRequest } from '../../api-helpers/api-helpers';
+import { useDispatch } from 'react-redux';
+import { userActions } from '../../store';
 
 const Auth = () => {
+  // Retorna a referencia para o dispatch da redux store
+  const dispatch = useDispatch()
   /*Recupera dos dados preenchidos no AuthForm para esse componente */
   /* Valida usuário com o backend */
   const getData = (data) => {
     sendUserAuthRequest(data.inputs, data.signup)
       .then((res) => console.log(res))
+      .then(() => dispatch(userActions.login()))
       .catch(err=>console.log);
 
   };
