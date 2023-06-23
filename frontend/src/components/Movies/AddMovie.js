59522501e6bcabafcea318ce398f9b2b7bd18a40
Kelvin Santos <kelvin.santos@valtech.com>
2023-06-22 21:34:37 -0300

    form da pagina de adicionar filme finalizado

--- frontend/src/components/Movies/AddMovie.js
+++ frontend/src/components/Movies/AddMovie.js
@@ -1,6 +1,9 @@
 import { Box, Button, Checkbox, FormLabel, TextField, Typography } from '@mui/material'
 import React from 'react'
-
+const labelProsp = {
+    mt: 1,
+    mb: 1,
+}
 const AddMovie = () => {
   return (
     <div>
@@ -9,21 +12,24 @@ const AddMovie = () => {
                 <Typography textAlign={"center"} variant='h5' fontFamily={"verdana"}>
                     Adicionar Novo Filme
                 </Typography>
-                <FormLabel>Titulo</FormLabel>
+                <FormLabel sx={{ labelProsp}}>Titulo</FormLabel>
                 <TextField name="title" variant='standard' margin='normal'></TextField>
-                <FormLabel>Descrição</FormLabel>
+                <FormLabel sx={{ labelProsp}} >Descrição</FormLabel>
                 <TextField name="description" variant='standard' margin='normal'></TextField>
-                <FormLabel>URL do Poster</FormLabel>
+                <FormLabel sx={{ labelProsp}} >URL do Poster</FormLabel>
                 <TextField name="poerUrl" variant='standard' margin='normal'></TextField>
-                <FormLabel>Data de Lançamento</FormLabel>
+                <FormLabel sx={{ labelProsp}} >Data de Lançamento</FormLabel>
                 <TextField name="releaseDate" variant='standard' margin='normal'></TextField>
-                <FormLabel>Atores</FormLabel>
+                <FormLabel sx={{ labelProsp}} >Atores</FormLabel>
                 <Box display={"flex"}>
                     <TextField name="actors" variant='standard' margin='normal'></TextField>
                     <Button>Adicionar</Button>
                 </Box>
-                <FormLabel>Featured</FormLabel>
+                <FormLabel sx={{ labelProsp}} >Featured</FormLabel>
                 <Checkbox sx={{mr:"auto"}}/>
+                <Button variant='contained' sx={{margin:"auto", width:"30%", bgcolor:"#2b2d42", ":hover":{bgcolor:"#121217"},}}> 
+                    Adicionar Novo Filme
+                </Button>
             </Box>
         </form>
     </div>
