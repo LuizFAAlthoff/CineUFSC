4aa21b5d21860f2ae87e06506bd9d88183d4ac81
Luiz Althoff <luizfernando.althoff@gmail.com>
2023-06-22 23:44:30 -0300

    delete movie

--- backend/routes/movie-routes.js
+++ backend/routes/movie-routes.js
@@ -6,6 +6,6 @@ const movieRouter = express.Router();
 movieRouter.get("/", getAllMovies);     // requisições GET no link /movie/ recebem de volta todos os filmes
 movieRouter.get("/:id", getMovieById);  // requisição GET no link /movie/*id do filme* que envia o id e recebe de volta o objeto filme
 movieRouter.post("/", addMovie);        // requisição POST no link /movie/ que envia os dados do filme e recebe de volta o id do novo filme
-userRouter.delete("/:id", deleteMovie); // requisição DELETE no link /movie/*id do filme* que envia o id do filme a ser excluído
+movieRouter.delete("/:id", deleteMovie); // requisição DELETE no link /movie/*id do filme* que envia o id do filme a ser excluído
 
 export default movieRouter;
